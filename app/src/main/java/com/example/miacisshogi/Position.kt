11b04970283a78324e61ce307596de9369f79ea4
sakoda <sakoda@keio.jp>
2021-03-23 17:21:50 +0900

    合法手生成を仮組み

--- app/src/main/java/com/example/miacisshogi/Position.kt
+++ app/src/main/java/com/example/miacisshogi/Position.kt
@@ -110,7 +110,7 @@ class Position {
             //ハッシュ値の更新
             //打つ前のHandHashとXORして消す
             val num = hand_[color_].num(kind(move.subject()))
-            hand_hash_ = hand_hash_ xor handHashSeed[color_][kind(move.subject())][num+1]
+            hand_hash_ = hand_hash_ xor handHashSeed[color_][kind(move.subject())][num + 1]
             //打った後のHandHashとXOR
             hand_hash_ = hand_hash_ xor handHashSeed[color_][kind(move.subject())][num]
             //打った後の分をXOR
@@ -133,9 +133,15 @@ class Position {
                 //取った駒分のハッシュをXOR
                 board_hash_ = board_hash_ xor hashSeed[move.capture()][to]
                 //増える前の持ち駒の分をXORして消す
-                hand_hash_ = hand_hash_ xor handHashSeed[color_][kind(move.capture())][hand_[color_].num(kind(move.capture()))-1]
+                hand_hash_ =
+                    hand_hash_ xor handHashSeed[color_][kind(move.capture())][hand_[color_].num(
+                        kind(move.capture())
+                    ) - 1]
                 //増えた後の持ち駒の分XOR
-                hand_hash_ = hand_hash_ xor handHashSeed[color_][kind(move.capture())][hand_[color_].num(kind(move.capture()))]
+                hand_hash_ =
+                    hand_hash_ xor handHashSeed[color_][kind(move.capture())][hand_[color_].num(
+                        kind(move.capture())
+                    )]
             }
 
             //成る手ならsubjectに成りのフラグを立てて,そうでないならsubjectをそのまま移動先に設置
@@ -188,7 +194,7 @@ class Position {
 
     //合法性に関する関数
     fun isLegalMove(move: Move): Boolean {
-        return true
+        return move in generateAllMoves()
     }
 
     fun isLastMoveDropPawn(): Boolean {
@@ -202,7 +208,7 @@ class Position {
     //詰み探索中に枝刈りして良いかを判定
     //王手がかかっていないときは枝刈りして良いだろう
     fun canSkipMateSearch(): Boolean {
-        return !is_checked_;
+        return !is_checked_
     }
 
     //この局面が詰み、千日手等で終わっているか確認する関数
@@ -226,15 +232,85 @@ class Position {
         //stringToMoveではどっちの手番かがわからない
         //つまりsubjectが完全には入っていないので手番付きの駒を入れる
         return if (move.isDrop()) {
-            dropMove(move.to(), if (color_ == BLACK) toBlack(move.subject()) else toWhite(move.subject()))
+            dropMove(
+                move.to(),
+                if (color_ == BLACK) toBlack(move.subject()) else toWhite(move.subject())
+            )
         } else {
-            return Move(move.to(), move.from(), false, move.isPromote(), board_[move.from().ordinal], board_[move.to().ordinal])
+            return Move(
+                move.to(),
+                move.from(),
+                false,
+                move.isPromote(),
+                board_[move.from().ordinal],
+                board_[move.to().ordinal]
+            )
         }
     }
 
     //合法手生成
     fun generateAllMoves(): ArrayList<Move> {
-        return ArrayList()
+        var move_buf = ArrayList<Move>()
+        for (sq in SquareList) {
+            if (board_[sq.ordinal] == EMPTY) {
+                //ここに打つ手が可能
+
+                //歩を打つ手
+                if (hand_[color_].num(PAWN) > 0) {
+                    //最奥の段は除外する
+                    if (color_ == BLACK && SquareToRank[sq.ordinal] <= Rank.Rank8 ||
+                        color_ == WHITE && SquareToRank[sq.ordinal] >= Rank.Rank2) {
+
+                        //2歩の除外
+                        var ok = true
+                        val file = SquareToFile[sq.ordinal].ordinal
+                        for (rank in Rank.Rank1.ordinal..Rank.Rank9.ordinal) {
+                            if (board_[FRToSquare[file][rank].ordinal] == coloredPiece(color_, PAWN)) {
+                                ok = false
+                                break
+                            }
+                        }
+                        if (ok) {
+                            pushMove(dropMove(sq, coloredPiece(color_, PAWN)), move_buf)
+                        }
+                    }
+                }
+
+                //香車
+                //最奥の段は除外する
+                if (hand_[color_].num(LANCE) > 0) {
+                    if (color_ == BLACK && SquareToRank[sq.ordinal] <= Rank.Rank8 ||
+                        color_ == WHITE && SquareToRank[sq.ordinal] >= Rank.Rank2) {
+                        pushMove(dropMove(sq, coloredPiece(color_, LANCE)), move_buf)
+                    }
+                }
+
+                //桂馬
+                //奥の2段は除外する
+                if (hand_[color_].num(KNIGHT) > 0) {
+                    if (color_ == BLACK && SquareToRank[sq.ordinal] <= Rank.Rank7 ||
+                        color_ == WHITE && SquareToRank[sq.ordinal] >= Rank.Rank3) {
+                        pushMove(dropMove(sq, coloredPiece(color_, KNIGHT)), move_buf)
+                    }
+                }
+
+                //その他
+                for (p in arrayListOf(SILVER, GOLD, BISHOP, ROOK)) {
+                    if (hand_[color_].num(p) > 0) {
+                        pushMove(dropMove(sq, coloredPiece(color_, p)), move_buf)
+                    }
+                }
+            } else if (pieceToColor(board_[sq.ordinal]) == color_) {
+                //この駒を動かす
+                val toList = movableSquareList(sq, board_[sq.ordinal])
+                for (to in toList) {
+                    //成る手が可能だったら先に生成
+                    val move = Move(to, sq, false, false, board_[sq.ordinal], board_[to.ordinal])
+                    pushMove(move, move_buf)
+                }
+            }
+        }
+        return move_buf
     }
 
     //sfenの入出力
@@ -248,41 +324,258 @@ class Position {
 
     //getter
     fun turnNumber(): Int {
-        return turn_number_;
+        return turn_number_
     }
 
     fun color(): Int {
-        return color_;
+        return color_
     }
 
     fun hashValue(): Long {
-        return hash_value_;
+        return hash_value_
     }
 
     fun on(sq: Square): Int {
-        return board_[sq.ordinal];
+        return board_[sq.ordinal]
     }
 
     fun isChecked(): Boolean {
-        return is_checked_;
+        return is_checked_
     }
 
     //--------------------
     //    内部メソッド
     //--------------------
+    private fun addSquare(sq: Square, list: ArrayList<Square>) {
+        if (board_[sq.ordinal] == EMPTY) {
+            return
+        }
+        list.add(sq)
+    }
+
+    private fun movableSquareList(sq: Square, color: Int, dir: Int): ArrayList<Square> {
+        val list = ArrayList<Square>()
+        var currSquare = SquareListWithWALL[sq.ordinal + dir]
+        while (true) {
+            if (board_[currSquare.ordinal] == EMPTY) {
+                //継続
+                list.add(currSquare)
+                currSquare = SquareListWithWALL[currSquare.ordinal + DIR_U]
+            } else if (board_[currSquare.ordinal] == WALL || pieceToColor(board_[currSquare.ordinal]) == color) {
+                //壁や自分側の駒の位置には突っ込めない
+                break
+            } else {
+                //相手の駒がある場合はここまで良し
+                list.add(currSquare)
+                break
+            }
+        }
+        return list
+    }
+
+    private fun movableSquareList(sq: Square, piece: Int): ArrayList<Square> {
+        //現状態について、sqマスにpieceがあると仮定したときに動かせるマスのリストを返す
+        val list = ArrayList<Square>()
+        when (piece) {
+            BLACK_PAWN -> {
+                addSquare(SquareListWithWALL[sq.ordinal + DIR_U], list)
+            }
+            BLACK_LANCE -> {
+                list.addAll(movableSquareList(sq, pieceToColor(piece), DIR_U))
+            }
+            BLACK_KNIGHT -> {
+                addSquare(SquareListWithWALL[sq.ordinal + DIR_RUU], list)
+                addSquare(SquareListWithWALL[sq.ordinal + DIR_LUU], list)
+            }
+            BLACK_SILVER -> {
+                addSquare(SquareListWithWALL[sq.ordinal + DIR_U], list)
+                addSquare(SquareListWithWALL[sq.ordinal + DIR_RU], list)
+                addSquare(SquareListWithWALL[sq.ordinal + DIR_RD], list)
+                addSquare(SquareListWithWALL[sq.ordinal + DIR_LD], list)
+                addSquare(SquareListWithWALL[sq.ordinal + DIR_LU], list)
+            }
+            BLACK_GOLD,
+            BLACK_PAWN_PROMOTE,
+            BLACK_LANCE_PROMOTE,
+            BLACK_KNIGHT_PROMOTE,
+            BLACK_SILVER_PROMOTE -> {
+                addSquare(SquareListWithWALL[sq.ordinal + DIR_U], list)
+                addSquare(SquareListWithWALL[sq.ordinal + DIR_RU], list)
+                addSquare(SquareListWithWALL[sq.ordinal + DIR_R], list)
+                addSquare(SquareListWithWALL[sq.ordinal + DIR_D], list)
+                addSquare(SquareListWithWALL[sq.ordinal + DIR_L], list)
+                addSquare(SquareListWithWALL[sq.ordinal + DIR_LU], list)
+            }
+            WHITE_PAWN -> {
+                addSquare(SquareListWithWALL[sq.ordinal + DIR_D], list)
+            }
+            WHITE_LANCE -> {
+                list.addAll(movableSquareList(sq, pieceToColor(piece), DIR_D))
+            }
+            WHITE_KNIGHT -> {
+                addSquare(SquareListWithWALL[sq.ordinal + DIR_RDD], list)
+                addSquare(SquareListWithWALL[sq.ordinal + DIR_LDD], list)
+            }
+            WHITE_SILVER -> {
+                addSquare(SquareListWithWALL[sq.ordinal + DIR_D], list)
+                addSquare(SquareListWithWALL[sq.ordinal + DIR_LD], list)
+                addSquare(SquareListWithWALL[sq.ordinal + DIR_LU], list)
+                addSquare(SquareListWithWALL[sq.ordinal + DIR_RU], list)
+                addSquare(SquareListWithWALL[sq.ordinal + DIR_RD], list)
+            }
+            WHITE_GOLD,
+            WHITE_PAWN_PROMOTE,
+            WHITE_LANCE_PROMOTE,
+            WHITE_KNIGHT_PROMOTE,
+            WHITE_SILVER_PROMOTE -> {
+                addSquare(SquareListWithWALL[sq.ordinal + DIR_D], list)
+                addSquare(SquareListWithWALL[sq.ordinal + DIR_LD], list)
+                addSquare(SquareListWithWALL[sq.ordinal + DIR_L], list)
+                addSquare(SquareListWithWALL[sq.ordinal + DIR_U], list)
+                addSquare(SquareListWithWALL[sq.ordinal + DIR_R], list)
+                addSquare(SquareListWithWALL[sq.ordinal + DIR_RD], list)
+            }
+            BLACK_BISHOP,
+            WHITE_BISHOP -> {
+                list.addAll(movableSquareList(sq, pieceToColor(piece), DIR_RU))
+                list.addAll(movableSquareList(sq, pieceToColor(piece), DIR_RD))
+                list.addAll(movableSquareList(sq, pieceToColor(piece), DIR_LD))
+                list.addAll(movableSquareList(sq, pieceToColor(piece), DIR_LU))
+            }
+            BLACK_BISHOP_PROMOTE,
+            WHITE_BISHOP_PROMOTE -> {
+                list.addAll(movableSquareList(sq, pieceToColor(piece), DIR_RU))
+                list.addAll(movableSquareList(sq, pieceToColor(piece), DIR_U))
+                list.addAll(movableSquareList(sq, pieceToColor(piece), DIR_U))
+                list.addAll(movableSquareList(sq, pieceToColor(piece), DIR_U))
+                addSquare(SquareListWithWALL[sq.ordinal + DIR_U], list)
+                addSquare(SquareListWithWALL[sq.ordinal + DIR_R], list)
+                addSquare(SquareListWithWALL[sq.ordinal + DIR_D], list)
+                addSquare(SquareListWithWALL[sq.ordinal + DIR_L], list)
+            }
+            BLACK_ROOK,
+            WHITE_ROOK -> {
+                list.addAll(movableSquareList(sq, pieceToColor(piece), DIR_U))
+                list.addAll(movableSquareList(sq, pieceToColor(piece), DIR_R))
+                list.addAll(movableSquareList(sq, pieceToColor(piece), DIR_D))
+                list.addAll(movableSquareList(sq, pieceToColor(piece), DIR_L))
+            }
+            BLACK_ROOK_PROMOTE,
+            WHITE_ROOK_PROMOTE -> {
+                list.addAll(movableSquareList(sq, pieceToColor(piece), DIR_U))
+                list.addAll(movableSquareList(sq, pieceToColor(piece), DIR_R))
+                list.addAll(movableSquareList(sq, pieceToColor(piece), DIR_D))
+                list.addAll(movableSquareList(sq, pieceToColor(piece), DIR_L))
+                addSquare(SquareListWithWALL[sq.ordinal + DIR_RU], list)
+                addSquare(SquareListWithWALL[sq.ordinal + DIR_RD], list)
+                addSquare(SquareListWithWALL[sq.ordinal + DIR_LD], list)
+                addSquare(SquareListWithWALL[sq.ordinal + DIR_LU], list)
+            }
+            BLACK_KING,
+            WHITE_KING -> {
+                addSquare(SquareListWithWALL[sq.ordinal + DIR_U], list)
+                addSquare(SquareListWithWALL[sq.ordinal + DIR_RU], list)
+                addSquare(SquareListWithWALL[sq.ordinal + DIR_R], list)
+                addSquare(SquareListWithWALL[sq.ordinal + DIR_RD], list)
+                addSquare(SquareListWithWALL[sq.ordinal + DIR_D], list)
+                addSquare(SquareListWithWALL[sq.ordinal + DIR_LD], list)
+                addSquare(SquareListWithWALL[sq.ordinal + DIR_L], list)
+                addSquare(SquareListWithWALL[sq.ordinal + DIR_LU], list)
+            }
+            else -> {
+            }
+        }
+        return list
+    }
+
+    private fun isThereControl(color: Int, sq: Square): Boolean {
+        //color側の利きがsqマスへあるかどうか
+        //sqマスからcolorとは逆側の手番として利きを生成
+        val oppColor = oppositeColor(color)
+        val pieceList = arrayListOf(
+            PAWN,
+            LANCE,
+            KNIGHT,
+            SILVER,
+            GOLD,
+            BISHOP,
+            ROOK,
+            KING,
+            PAWN_PROMOTE,
+            LANCE_PROMOTE,
+            KNIGHT_PROMOTE,
+            SILVER_PROMOTE,
+            BISHOP_PROMOTE,
+            ROOK_PROMOTE
+        )
+        for (piece in pieceList) {
+            for (s in movableSquareList(sq, coloredPiece(oppColor, piece))) {
+                if (board_[s.ordinal] == coloredPiece(color, piece)) {
+                    return true
+                }
+            }
+        }
+        return false
+    }
+
     //合法手生成で用いる関数
-    fun canPromote(move: Move): Boolean {
-        return false
+    private fun canPromote(move: Move): Boolean {
+        val to = move.to().ordinal
+        val from = move.from().ordinal
+        if (move.isDrop()                        //打つ手だったらダメ
+            || board_[from] and PROMOTE != 0     //すでに成っている駒を動かす手だったらダメ
+            || kind(board_[from]) == GOLD        //動かす駒が金だったらダメ
+            || kind(board_[from]) == KING        //動かす駒が玉だったらダメ
+        ) {
+            return false
+        }
+
+        //位置関係
+        if (color_ == BLACK) {
+            return ((Rank.Rank1 <= SquareToRank[to] && SquareToRank[to] <= Rank.Rank3) ||
+                    (Rank.Rank1 <= SquareToRank[from] && SquareToRank[from] <= Rank.Rank3))
+        } else {
+            return ((Rank.Rank7 <= SquareToRank[to] && SquareToRank[to] <= Rank.Rank9) ||
+                    (Rank.Rank7 <= SquareToRank[from] && SquareToRank[from] <= Rank.Rank9))
+        }
     }
 
-    fun canDropPawn(to: Square): Boolean {
-        return false
-    }
+    private fun pushMove(move: Move, move_buf: ArrayList<Move>) {
+        val moveColor = color_
 
-    fun pushMove(move: Move, move_buf: ArrayList<Move>) {}
-    fun generateNormalMoves(move_buf: ArrayList<Move>) {}
-    fun generateEvasionMoves(move_buf: ArrayList<Move>) {}
-//    void generateDropMoves(const Bitboard& to_bb, std::vector<Move>& move_buf) const;
+        //動かしてみる
+        doMove(move)
+
+        //TODO:ここで打ち歩判定も入れるべきだな
+
+        //自玉に王手がかかったまま, あるいは動かしたことで王手になるとダメ
+        val ok = !isThereControl(color_, king_sq_[moveColor])
+
+        undo()
+
+        if (!ok) {
+            return
+        }
+
+        //成る手が可能だったら先に生成
+        if (canPromote(move)) {
+            move_buf.add(promotiveMove(move))
+            val to = move.to().ordinal
+            when (kind(board_[move.from().ordinal])) {
+                //香、桂は位置によっては成る手しか不可
+                LANCE -> {
+                    if (color_ == BLACK && SquareToRank[to] <= Rank.Rank1) return
+                    if (color_ == WHITE && SquareToRank[to] >= Rank.Rank9) return
+                }
+                KNIGHT -> {
+                    if (color_ == BLACK && SquareToRank[to] <= Rank.Rank2) return
+                    if (color_ == WHITE && SquareToRank[to] >= Rank.Rank8) return
+                }
+            }
+        }
+        //成らない手を追加
+        move_buf.add(move)
+    }
 
     //ハッシュ値の初期化
     fun initHashValue() {}
@@ -295,8 +588,8 @@ class Position {
     //    クラス変数
     //------------------
     //ハッシュの各駒・位置に対する決められた値
-    private val hashSeed = Array(PieceNum) { Array(Square.SquareNum.ordinal){ Random.nextLong() } }
-    private val handHashSeed = Array(ColorNum) { Array(PieceNum){ Array(19){ Random.nextLong() } } }
+    private val hashSeed = Array(PieceNum) { Array(Square.SquareNum.ordinal) { Random.nextLong() } }
+    private val handHashSeed = Array(ColorNum) { Array(PieceNum) { Array(19) { Random.nextLong() } } }
 
     //------------------------
     //    インスタンス変数
